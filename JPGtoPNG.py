3b19df037652c9c328cfa98f537ab17b0e0ecf30
SEWOONG LEE <dltpdnd1234@gmail.com>
2024-01-23 17:33:36 -0500

    Test Commit

--- JPGtoPNG.py
+++ JPGtoPNG.py
@@ -7,3 +7,23 @@ from PIL import Image
 
 images = ['test.png']
 
+
+for infile in images:
+
+    f, e = os.path.splitext(infile)
+
+    outfile = f + '.jpg'
+
+    if infile != outfile:
+
+        try:
+
+            with Image.open(infile) as image:
+
+                in_rgb = image.convert('RGB')
+
+                in_rgb.save(outfile, 'JPEG')
+
+        except OSError:
+
+            print('Conversion failed for', infile)
